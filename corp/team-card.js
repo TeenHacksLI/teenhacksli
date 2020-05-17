e38adff426bb36b6ef26c5df5e89e5fa7f34eb41
jeffreyyu101 <jeffreyyu101@gmail.com>
2020-05-17 18:12:42 -0400

    minor changes

--- corp/team-card.js
+++ corp/team-card.js
@@ -5,7 +5,7 @@ const buildTeamCard = team => {
   // can be appended to it
   const body = document.querySelector("."+team.com);
 
-  body.innerHTML += "<div class=\"col-6\"><div class=\"row mb-3\"><div class=\"col-4\"><img src=\"img/team/".concat(team.FirstName,"_",team.LastName,".png\"></div><div class=\"col-8\"><h3>",team.FirstName," ",team.LastName,"<br><em><small>",team.title,"</small></em></h3><h5>",team.school,"</h5><a href=\"mailto:",team.email,"\"><i class=\"fas fa-envelope fa-2x mr-2\"></i></a><a href=\"",team.linkedin,"\" target=\"_blank\"><i class=\"fab fa-linkedin fa-2x\"></i></a></div></div></div>");
+  body.innerHTML += "<div class=\"col-md-6\"><div class=\"row mb-3\"><div class=\"col-4\"><img src=\"img/team/".concat(team.FirstName,"_",team.LastName,".png\"></div><div class=\"col-8\"><h3>",team.FirstName," ",team.LastName,"<br><em><small>",team.title,"</small></em></h3><h5>",team.school,"</h5><a href=\"mailto:",team.email,"\"><i class=\"fas fa-envelope fa-2x mr-2\"></i></a><a href=\"",team.linkedin,"\" target=\"_blank\"><i class=\"fab fa-linkedin fa-2x\"></i></a></div></div></div>");
 
 }
 
