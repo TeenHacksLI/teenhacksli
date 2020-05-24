ed485338ae94636044745b9102633ecbc17aaf61
Jeffrey Yu <jeffreyyu101@gmail.com>
2020-05-23 20:22:22 -0400

    test

---
+++ corp/email.js
@@ -0,0 +1,2 @@
+
+(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';}(jQuery));var $mcj = jQuery.noConflict(true);
